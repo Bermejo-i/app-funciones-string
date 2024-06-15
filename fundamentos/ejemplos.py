4de0bf69244e779e7df6482fdbd25110c0c584af
Bermejo-i <israelbermejo.1@gmail.com>
2024-06-14 19:58:38 -0500

    semana 4

---
+++ fundamentos/ejemplos.py
@@ -0,0 +1,19 @@
+
+def mayusculaMinuscula(valor):
+    print(valor.upper())
+    print(valor.lower())
+    print(valor.capitalize())
+    print(len(valor))
+
+def longMensaje(valor):
+    print("la longitud del string es: ", len(valor))
+
+def convertNum2str(valor):
+    print(str(valor))
+
+def arregloPalabras(valor):
+    print(valor.split())
+
+def array2str():
+    nombres = ["Luis","Angel","Maria","Angelica"]
+    print()
